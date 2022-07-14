35f07e926807d0c889e4d61dff8bc155bc28db5b
Kaueazx <kaueaexzplosion@hotmail.com>
2022-07-14 11:00:31 -0300

    inicializacao dos destaques

---
+++ src/Destaques/Destaques.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import "./Destaques.css"
+
+const Destaques = () => {
+    return ( 
+        <div className='Destaques'>
+            destaques
+        </div>
+    );
+}
+ 
+export default Destaques;
--- src/Menu/Menu.js
+++ src/Menu/Menu.js
@@ -1,22 +1,38 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './Menu.css'
 
 import logo from "../assets/images/logo.png"
 import {ReactComponent as Blocks} from "../assets/svg/blocks.svg"
 import {ReactComponent as List} from "../assets/svg/list.svg"
+import {ReactComponent as Hamburger} from "../assets/svg/hamburger.svg"
+import {ReactComponent as Sun} from "../assets/svg/sun.svg"
 import Switch from '../Switch/Switch'
 
 
 const Menu = () => {
+    const[viewStyle, setViewStyle] = useState(1);
     return ( 
         <div className="Menu">
+            <div className="Menu-hamburger">
+                <Hamburger />
+            </div>
+
             <div className="Menu-left">
-                <Blocks className="selected"/>
-                <List />
+                
+                <Blocks 
+                    className={viewStyle === 0 ? 'selected' : ""}
+                    onClick={() => {setViewStyle(0)}}
+                />
+                
+                <List 
+                    className={viewStyle === 1 ? 'selected' : ""} 
+                    onClick={() => {setViewStyle(1)}}
+                />
             </div>
 
             <img src={logo} className="Menu-logo" />
             <div className="Menu-right">
+                <Sun/>
                 <Switch />
             </div>
 
