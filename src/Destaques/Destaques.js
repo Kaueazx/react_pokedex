922e9832f06a02d4d2780eeff366c00363567698
Kaueazx <kaueaexzplosion@hotmail.com>
2022-07-19 10:35:55 -0300

    sessao destaques

--- src/Destaques/Destaques.js
+++ src/Destaques/Destaques.js
@@ -1,10 +1,15 @@
 import React from 'react';
+import ReloadButton from '../ReloadButton/ReloadButton';
 import "./Destaques.css"
 
 const Destaques = () => {
+    
+
     return ( 
         <div className='Destaques'>
-            destaques
+           <h1> Destaques </h1>
+
+           <ReloadButton />
         </div>
     );
 }
